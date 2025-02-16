499d9270184cd43bddc32808c2f2cdb67ca8d479
montehurd <mhurd@wikimedia.org>
2025-02-15 18:22:24 -0600

    simplify duration bits

--- git-cache/git-cache-operation.sh
+++ git-cache/git-cache-operation.sh
@@ -6,16 +6,18 @@ REPO_PATH="$1"
 REPO_URL="$2"
 PROGRESS_LOG="$3"
 
+start=$(date +%s)
+
 if [ ! -d "$REPO_PATH" ]; then
-    echo -e "\nStarting cache git clone of $REPO_URL" >> "$PROGRESS_LOG"
-    start=$(date +%s)
-    git clone --bare --no-tags --progress "$REPO_URL" "$REPO_PATH" 2>> "$PROGRESS_LOG"
-    end=$(date +%s)
-    echo "Finished cache git clone (took $((end - start)) seconds)" >> "$PROGRESS_LOG"
+   echo -e "\nStarting cache git clone of '$REPO_URL'" >> "$PROGRESS_LOG"
+   git clone --bare --no-tags --progress "$REPO_URL" "$REPO_PATH" 2>> "$PROGRESS_LOG"
+   echo "Finished cache git clone" >> "$PROGRESS_LOG"
 else
-    echo -e "\nStarting cache git fetch of $REPO_URL" >> "$PROGRESS_LOG"
-    start=$(date +%s)
-    cd "$REPO_PATH" && git fetch --no-tags --progress 2>> "$PROGRESS_LOG"
-    end=$(date +%s)
-    echo "Finished cache git fetch (took $((end - start)) seconds)" >> "$PROGRESS_LOG"
+   echo -e "\nStarting cache git fetch of '$REPO_URL'" >> "$PROGRESS_LOG"
+   cd "$REPO_PATH" && git fetch --no-tags --progress 2>> "$PROGRESS_LOG"
+   echo "Finished cache git fetch" >> "$PROGRESS_LOG"
 fi
+
+end=$(date +%s)
+
+echo "Duration: $((end - start)) seconds" >> "$PROGRESS_LOG"
